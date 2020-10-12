68c9d8ece00df9584ed631973010f706be4296c6
mdrobinson4 <mdrobinson4>
2020-10-12 15:40:44 -0400

    update

--- catkin_ws/src/imu_pp/src/imu_pp/deadReckoning.py
+++ catkin_ws/src/imu_pp/src/imu_pp/deadReckoning.py
@@ -1,4 +1,5 @@
 from filterpy.kalman import KalmanFilter
+from scipy.spatial.transform import Rotation as R
 import numpy as np
 
 class DeadReckoning:
@@ -7,9 +8,12 @@ class DeadReckoning:
 
 
     def updatePose(linearAccel, angularVel, quaternion):
-        z = np.stack((linearAccel, angularVel, orientation), axis=0)
+        r = R.from_quat(quaternion)
+        euler = r.as_euler('xyz', degrees=False)
+        z = np.stack((linearAccel, angularVel, euler), axis=0)
         self.kalmanFilter.predict()
         self.kalmanFilter.update(z)
+        self.pose = self.kalmanFilter.x
 
 
     def get_status(self):
